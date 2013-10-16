66b45cfc643b6d9b7e5f20280b14f0e3d6c51062
cewing <cris@crisewing.com>
2013-10-15 18:51:33 -0700

    add a method for creating a zip archive.  It will take the output path and the filehandle for the recently exported xml from PHP.

--- src/rcr_export_control/utils.py
+++ src/rcr_export_control/utils.py
@@ -1,6 +1,25 @@
 # -*- coding: utf-8 -*-
+from lxml import etree
+from rcr_export_control.xml_tools import parse_export_xml
+from rcr_export_control.xml_tools import exerpt_body_content
+from rcr_export_control.xml_tools import get_base_file_id
+from rcr_export_control.xml_tools import convert_file_id_to_article_id
+from rcr_export_control.xml_tools import convert_galleys
+from rcr_export_control.xml_tools import convert_supplemental_files
+from subprocess import Popen
+from subprocess import PIPE
+from subprocess import CalledProcessError
+
 import os
 import sys
+import zipfile
+
+try:
+    import zlib
+    compression = zipfile.ZIP_DEFLATED
+except:
+    compression = zipfile.ZIP_STORED
+
 
 class MissingBinary(Exception): 
     pass
@@ -73,3 +92,47 @@ def bin_search(binary, default=_marker):
                            (binary, os.pathsep.join(bin_search_path)))
     else:
         return default
+
+
+def execute_php_export(command, articleid):
+    print "PHP Exporting article {0}:\n\t`$ {1}\n`".format(articleid, command)
+    args = command.split()
+    process = Popen(args, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    pout, perr = process.communicate()
+    code = process.poll()
+    if code or pout or perr:
+        output = pout + perr
+        try:
+            raise CalledProcessError(
+                code, command, output=output
+            )
+        except Exception:
+            error = CalledProcessError(code, command)
+            error.output = output
+            raise error
+    print "PHP export of article {0} complete\n".format(articleid)
+    return code
+
+
+def create_article_archive(out_path, exported):
+    import pdb; pdb.set_trace( )
+    parsed = parse_export_xml(exported)
+    base_filename = get_base_file_id(parsed)
+    inner_basename = convert_file_id_to_article_id(base_filename)
+    markup_node, galleys_node, supplemental_node = exerpt_body_content(parsed)
+    galleys = []
+    supp_files = []
+    if galleys_node is not None:
+        galleys = convert_galleys(galleys_node)
+    if supplemental_node is not None:
+        supp_files = convert_supplemental_files(supplemental_node)
+    archive_name = base_filename + '.zip'
+    archive_path = os.path.join(out_path, archive_name)
+    archive = zipfile.ZipFile(archive_path, 'w', compression=compression)
+    xml_filename = inner_basename + '.xml'
+    archive.writestr(
+        xml_filename, etree.tostring(
+            parsed, encoding='utf-8', xml_declaration=True
+        )
+    )
+    archive.close()
